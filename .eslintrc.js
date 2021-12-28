9c04c52ce7caab9fdd4cba0793cd591fa7a338b1
Evan Sutherland <stackoverfloweth@gmail.com>
2021-12-28 15:49:16 -0600

    turning off eslint no-extra-parens rule when in ts or vue file

--- .eslintrc.js
+++ .eslintrc.js
@@ -231,6 +231,12 @@ module.exports = {
         'indent': 'off',
         '@typescript-eslint/indent': 'off'
       }
+    },
+    {
+      files: ['**/*.vue', '**/*.ts'],
+      rules: {
+        'no-extra-parens': 'off'
+      }
     }
   ]
 }
